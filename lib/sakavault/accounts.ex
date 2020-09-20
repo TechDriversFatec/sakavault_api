c93d2963aafc9b3d2c63de61edfd30db3a8d9635
Eduardo Maia <eduvimaia@gmail.com>
2020-09-20 18:02:36 -0300

    accounts to find user by email

--- lib/sakavault/accounts.ex
+++ lib/sakavault/accounts.ex
@@ -2,10 +2,15 @@ defmodule SakaVault.Accounts do
   @moduledoc false
 
   alias SakaVault.Accounts.User
+  alias SakaVault.Fields.Hash
   alias SakaVault.Repo
 
   def find(id) do
     {:ok, Repo.get(User, id)}
+  end
+
+  def find_by_email(email) do
+    {:ok, Repo.get_by(User, email_hash: Hash.hash(email))}
   end
 
   def create(attrs \\ %{}) do
--- test/sakavault/accounts_test.exs
+++ test/sakavault/accounts_test.exs
@@ -2,30 +2,29 @@ defmodule SakaVault.AccountsTest do
   use SakaVault.DataCase
 
   alias SakaVault.Accounts
-
   alias SakaVault.Accounts.User
 
+  setup do
+    {:ok, user: insert(:user)}
+  end
+
   describe "find/1" do
-    setup do
-      {:ok, user} =
-        %{
-          name: "John Doe",
-          email: "john@doe.com",
-          password: "johndoe123"
-        }
-        |> Accounts.create()
-
-      {:ok, user: user}
-    end
-
     test "with valid id", %{user: user} do
       assert {:ok, %User{} = user} = Accounts.find(user.id)
-
-      assert user.name == "John Doe"
     end
 
     test "with invalid id" do
       assert {:ok, nil} = Accounts.find(Ecto.UUID.generate())
+    end
+  end
+
+  describe "find_by_email/1" do
+    test "with valid email", %{user: user} do
+      assert {:ok, %User{} = user} = Accounts.find_by_email(user.email)
+    end
+
+    test "with invalid email" do
+      assert {:ok, nil} = Accounts.find_by_email("invalid@user.com")
     end
   end
 
