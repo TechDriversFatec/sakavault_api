9cea536328271a95b0b75b0335a6460d6ac101f6
Eduardo Maia <eduvimaia@gmail.com>
2020-09-20 16:03:24 -0300

    accounts can now search for specific users by id

--- lib/sakavault/accounts.ex
+++ lib/sakavault/accounts.ex
@@ -4,6 +4,10 @@ defmodule SakaVault.Accounts do
   alias SakaVault.Accounts.User
   alias SakaVault.Repo
 
+  def find(id) do
+    {:ok, Repo.get(User, id)}
+  end
+
   def create(attrs \\ %{}) do
     attrs
     |> User.changeset()
--- test/sakavault/accounts_test.exs
+++ test/sakavault/accounts_test.exs
@@ -5,6 +5,30 @@ defmodule SakaVault.AccountsTest do
 
   alias SakaVault.Accounts.User
 
+  describe "find/1" do
+    setup do
+      {:ok, user} =
+        %{
+          name: "John Doe",
+          email: "john@doe.com",
+          password: "johndoe123"
+        }
+        |> Accounts.create()
+
+      {:ok, user: user}
+    end
+
+    test "with valid id", %{user: user} do
+      assert {:ok, %User{} = user} = Accounts.find(user.id)
+
+      assert user.name == "John Doe"
+    end
+
+    test "with invalid id" do
+      assert {:ok, nil} = Accounts.find(Ecto.UUID.generate())
+    end
+  end
+
   describe "create/1" do
     test "with valid data creates a user" do
       attrs = %{
