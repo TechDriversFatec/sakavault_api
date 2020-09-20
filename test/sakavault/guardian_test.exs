923e8679b48e6f46887bdd25a4e904e1816bbd3c
Eduardo Maia <eduvimaia@gmail.com>
2020-09-20 16:04:37 -0300

    mix format

--- test/sakavault/guardian_test.exs
+++ test/sakavault/guardian_test.exs
@@ -22,11 +22,12 @@ defmodule SakaVault.GuardianTest do
 
   describe "resource_from_claims/2" do
     test "with valid claims" do
-      {:ok, %{id: user_id} = user} = Accounts.create(%{
-        name: "John Doe",
-        password: "johndoe",
-        email: "john@doe.com"
-      })
+      {:ok, %{id: user_id} = user} =
+        Accounts.create(%{
+          name: "John Doe",
+          password: "johndoe",
+          email: "john@doe.com"
+        })
 
       assert {:ok, %{id: ^user_id}} = Guardian.resource_from_claims(%{"sub" => user.id})
     end
