24907b107722d4b50872ffec65d70c3aad47f567
Eduardo Maia <eduvimaia@gmail.com>
2020-09-20 14:45:44 -0300

    using custom ecto types for password, hash and encrypted fields in user

--- lib/sakavault/accounts/user.ex
+++ lib/sakavault/accounts/user.ex
@@ -3,19 +3,41 @@ defmodule SakaVault.Accounts.User do
 
   use SakaVault.Schema
 
+  alias SakaVault.Fields.{
+    Encrypted,
+    Hash,
+    Password
+  }
+
   schema "users" do
-    field :name, :binary
-    field :email, :binary
-    field :email_hash, :binary
-    field :password_hash, :binary
+    field :name, Encrypted
+    field :email, Encrypted
+    field :email_hash, Hash
+
+    field :password, :binary, virtual: true
+    field :password_hash, Password
 
     timestamps()
   end
 
   def changeset(%__MODULE__{} = user, params) do
     user
-    |> cast(params, [:name, :email, :email_hash, :password_hash])
-    |> validate_required([:name, :email, :email_hash, :password_hash])
+    |> cast(params, [:name, :email, :password])
+    |> validate_required([:name, :email, :password])
+    |> add_email_hash()
+    |> add_password_hash()
     |> unique_constraint(:email_hash)
   end
+
+  defp add_email_hash(%{changes: %{email: email}} = changeset) do
+    put_change(changeset, :email_hash, email)
+  end
+
+  defp add_email_hash(changeset), do: changeset
+
+  defp add_password_hash(%{changes: %{password: password}} = changeset) do
+    put_change(changeset, :password_hash, password)
+  end
+
+  defp add_password_hash(changeset), do: changeset
 end
