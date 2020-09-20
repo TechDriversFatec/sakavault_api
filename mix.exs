729857daf3f652610bced5e98714d848d980fbd4
Eduardo Maia <eduvimaia@gmail.com>
2020-09-20 15:43:52 -0300

    added guardian

--- mix.exs
+++ mix.exs
@@ -46,6 +46,9 @@ defmodule SakaVault.MixProject do
       # General
       {:jason, "~> 1.0"},
 
+      # Auth
+      {:guardian, "~> 2.1"},
+
       # Securely hashing & verifying passwords
       {:argon2_elixir, "~> 1.3"},
 
