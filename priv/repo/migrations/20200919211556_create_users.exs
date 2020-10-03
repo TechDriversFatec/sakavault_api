c02d23cda6673d2c0910685962695be36276b450
Eduardo Maia <eduvimaia@gmail.com>
2020-10-03 14:11:52 -0300

    tablename in atom

--- priv/repo/migrations/20200919211556_create_users.exs
+++ priv/repo/migrations/20200919211556_create_users.exs
@@ -2,7 +2,7 @@ defmodule SakaVault.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
-    create table("users") do
+    create table(:users) do
       add :name, :binary, null: false
       add :email, :binary, null: false
       add :email_hash, :binary, null: false
