d3e848762ff456e456cbc6337b64e7f60fcba75d
Eduardo Maia <eduvimaia@gmail.com>
2020-10-03 14:37:45 -0300

    mix format

--- test/sakavault/vault/secret_test.exs
+++ test/sakavault/vault/secret_test.exs
@@ -14,16 +14,18 @@ defmodule SakaVault.Vault.SecretTest do
   end
 
   test "changeset with valid attributes", %{user: user} do
-    assert %{valid?: true} = @valid_attrs
-                             |> Map.merge(%{user_id: user.id})
-                             |> Secret.changeset()
+    assert %{valid?: true} =
+             @valid_attrs
+             |> Map.merge(%{user_id: user.id})
+             |> Secret.changeset()
   end
 
   test "changeset with valid attributes and invalid user" do
-    assert {:error, %{valid?: false}} = @valid_attrs
-                                        |> Map.merge(%{user_id: Ecto.UUID.generate()})
-                                        |> Secret.changeset()
-                                        |> Repo.insert()
+    assert {:error, %{valid?: false}} =
+             @valid_attrs
+             |> Map.merge(%{user_id: Ecto.UUID.generate()})
+             |> Secret.changeset()
+             |> Repo.insert()
   end
 
   test "changeset with invalid attributes", %{user: user} do
