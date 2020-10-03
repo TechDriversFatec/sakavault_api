1fa25a7fe2b4fcf01229d78e218b9110b26fb64e
Eduardo Maia <eduvimaia@gmail.com>
2020-10-03 14:14:52 -0300

    created secrets table migration

---
+++ priv/repo/migrations/20201003171124_create_secrets.exs
@@ -0,0 +1,16 @@
+defmodule SakaVault.Repo.Migrations.CreateSecrets do
+  use Ecto.Migration
+
+  def change do
+    create table(:secrets) do
+      add :name, :binary, null: false
+      add :notes, :binary, null: false
+      add :username, :binary, null: false
+      add :password, :binary, null: false
+
+      add :user_id, references(:users, type: :uuid, on_delete: :delete_all), null: false
+
+      timestamps()
+    end
+  end
+end
