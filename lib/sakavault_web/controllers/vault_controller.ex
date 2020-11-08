d590468d2a2e94e13fce474ab3cf5e022ac2cb5e
Eduardo Maia <eduvimaia@gmail.com>
2020-11-08 16:51:08 -0300

    vault encryption

--- lib/sakavault_web/controllers/vault_controller.ex
+++ lib/sakavault_web/controllers/vault_controller.ex
@@ -3,7 +3,7 @@ defmodule SakaVaultWeb.VaultController do
 
   action_fallback SakaVaultWeb.FallbackController
 
-  alias SakaVault.Vault
+  alias SakaVault.{Krypto, Vault}
   alias SakaVault.Vault.Secret
 
   def action(conn, _) do
@@ -17,26 +17,33 @@ defmodule SakaVaultWeb.VaultController do
   end
 
   def index(conn, _params, user) do
-    secrets = Vault.all(user)
+    secrets =
+      user
+      |> Vault.all()
+      |> Enum.map(&decrypt_secret/1)
+      |> Enum.map(fn {_, secret} -> secret end)
 
     render(conn, "index.json", secrets: secrets)
   end
 
   def show(conn, params, _user) do
-    {:ok, secret} = Vault.find(params["id"])
-
-    render(conn, "show.json", %{secret: secret})
+    with {:ok, secret} <- Vault.find(params["id"]),
+         {:ok, secret} <- decrypt_secret(secret) do
+      render(conn, "show.json", %{secret: secret})
+    end
   end
 
   def create(conn, params, user) do
-    with {:ok, secret} <- Vault.create(user, params) do
+    with {:ok, %Secret{} = secret} <- Vault.create(user, params),
+         {:ok, %Secret{} = secret} <- decrypt_secret(secret) do
       render(conn, "show.json", %{secret: secret})
     end
   end
 
   def update(conn, params, _user) do
     with {:ok, %Secret{} = secret} <- Vault.find(params["id"]),
-         {:ok, %Secret{} = secret} <- Vault.update(secret, params) do
+         {:ok, %Secret{} = secret} <- Vault.update(secret, params),
+         {:ok, %Secret{} = secret} <- decrypt_secret(secret) do
       render(conn, "show.json", %{secret: secret})
     end
   end
@@ -47,4 +54,7 @@ defmodule SakaVaultWeb.VaultController do
       render(conn, "delete.json", %{secret: secret})
     end
   end
+
+  defp decrypt_secret(nil), do: {:ok, nil}
+  defp decrypt_secret(%Secret{} = secret), do: {:ok, Krypto.decrypt(secret)}
 end
