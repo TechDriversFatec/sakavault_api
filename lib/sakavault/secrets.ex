95d5d89609a9fb0d4bd3161d546f6ecfe529200e
Eduardo Maia <eduvimaia@gmail.com>
2020-11-08 21:19:30 -0300

    secrets api can now delete a secret and once it's tried to fetch an invalid secret, it's return a not_found error

--- lib/sakavault/secrets.ex
+++ lib/sakavault/secrets.ex
@@ -2,9 +2,9 @@ defmodule SakaVault.Secrets do
   @moduledoc false
 
   def fetch(secret_id) do
-    {:ok, %{"SecretString" => secret_key}} = secrets_api().fetch(secret_id)
-
-    {:ok, secret_key}
+    secret_id
+    |> secrets_api().fetch()
+    |> parse_result()
   end
 
   def create(secret_id, secret_key) do
@@ -20,8 +20,21 @@ defmodule SakaVault.Secrets do
 
     secrets
     |> Enum.map(& &1["Name"])
+    |> Enum.map(&sanitize_secret_id/1)
     |> Enum.each(&delete/1)
   end
 
-  def secrets_api, do: Application.get_env(:sakavault, :secrets_api)
+  defp sanitize_secret_id(name) do
+    name
+    |> String.split("/")
+    |> List.last()
+  end
+
+  defp parse_result({:ok, %{"SecretString" => secret_key}}), do: {:ok, secret_key}
+
+  defp parse_result({:error, {:http_error, 400, _}}), do: {:error, :not_found}
+
+  defp parse_result(result), do: result
+
+  defp secrets_api, do: Application.get_env(:sakavault, :secrets_api)
 end
